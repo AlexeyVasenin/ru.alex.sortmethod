e9d453883aac44db0422e03e842e679435519d98
Alexey <alexxx1809@gmail.com>
2021-04-02 15:57:26 +0500

    init

--- src/main/java/ru/alex/testsort/Main.java
+++ src/main/java/ru/alex/testsort/Main.java
@@ -1,7 +1,16 @@
 package ru.alex.testsort;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Main {
+    InputStream in;
+    try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in))) {
+
+    }
     public static void main(String[] args) {
 
+        switch ()
     }
 }
