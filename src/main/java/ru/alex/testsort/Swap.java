9ba668ba0c96b0b800d502d9e0d70a37689a35df
Alexey <alexxx1809@gmail.com>
2021-04-29 12:05:22 +0500

    newbranch test

--- src/main/java/ru/alex/testsort/Swap.java
+++ src/main/java/ru/alex/testsort/Swap.java
@@ -5,7 +5,10 @@ public class Swap {
     public static void swap(Integer[] array, int int1, int int2) {
         Integer tmp = array[int1];
         array[int1] = array[int2];
+
         array[int2] = tmp;
+        System.out.println("test branch");
     }
+
 }
 
